c4250faf085ab0dded97ff5eb53bab132adc149b
Mario <luix30lp@gmail.com>
2022-10-18 22:22:05 -0500

    Added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
 
 const Home: NextPage = () => {
-  return <h1>Hello</h1>;
+  return (
+    <ShopLayout
+      title={'Teslo-shop - Home'}
+      pageDescription={'Find the best products'}
+    >
+      <Typography variant="h1" component="h1">
+        Store
+      </Typography>
+      <Typography variant="h2" component="h2">
+        All products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
