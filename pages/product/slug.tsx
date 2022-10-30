4dc19caaf032d0b00e5560782ba718c9ad1df633
Mario <luix30lp@gmail.com>
2022-10-30 15:35:02 -0600

    Added size selector

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Chip, Grid, Typography } from '@mui/material';
 import { ShopLayout } from '../../components/layouts/ShopLayout';
-import { ProductSlideshow } from '../../components/products';
+import { ProductSlideshow, SizeSelector } from '../../components/products';
 import { ItemCounter } from '../../components/ui';
 import { initialData } from '../../database/products';
 
@@ -26,6 +26,10 @@ const ProductPage = () => {
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Quantity</Typography>
               <ItemCounter />
+              <SizeSelector
+                selectedSize={product.sizes[3]}
+                sizes={product.sizes}
+              />
             </Box>
 
             <Button color="secondary" className="circular-btn">
--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -1,33 +1,35 @@
 import { createTheme } from '@mui/material/styles';
-import { red,  } from '@mui/material/colors';
-
+import { red } from '@mui/material/colors';
 
 export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '##1E1E1E'
+      main: '#1E1E1E'
     },
     secondary: {
       main: '#3A64D8'
+    },
+    info: {
+      main: '#fff'
     }
   },
   components: {
     MuiLink: {
       defaultProps: {
-        underline: 'none',
-      },
+        underline: 'none'
+      }
     },
     MuiAppBar: {
       defaultProps: {
         elevation: 0,
-        position: 'fixed',
+        position: 'fixed'
       },
       styleOverrides: {
         root: {
           backgroundColor: 'white',
           height: 60
-        },
+        }
       }
     },
 
@@ -48,26 +50,25 @@ export const lightTheme = createTheme({
       }
     },
 
-
     MuiButton: {
       defaultProps: {
         variant: 'contained',
         size: 'small',
         disableElevation: true,
+        color: 'info'
       },
       styleOverrides: {
         root: {
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
-          ":hover": {
+          ':hover': {
             backgroundColor: 'rgba(0,0,0,0.05)',
             transition: 'all 0.3s ease-in-out'
           }
         }
       }
     },
-
 
     MuiCard: {
       defaultProps: {
@@ -76,10 +77,9 @@ export const lightTheme = createTheme({
       styleOverrides: {
         root: {
           boxShadow: '0px 5px 5px rgba(0,0,0,0.05)',
-          borderRadius: '10px',
+          borderRadius: '10px'
         }
       }
     }
-    
   }
 });
