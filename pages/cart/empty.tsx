1c653c46eb9ee7e02f9baf13155d517dc5333be9
Mario <luix30lp@gmail.com>
2022-10-30 15:41:38 -0600

    Added empty cart page

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,33 @@
+import { RemoveShoppingCartOutlined } from '@mui/icons-material';
+import { Box, Link, Typography } from '@mui/material';
+import { ShopLayout } from '../../components/layouts/ShopLayout';
+import NextLink from 'next/link';
+
+const EmptyPage = () => {
+  return (
+    <ShopLayout
+      title="Empty cart"
+      pageDescription="There is no products in your cart"
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+      >
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography>Your cart is empty</Typography>
+          <NextLink href="/" passHref>
+            <Link typography="h4" color="secondary">
+              Home
+            </Link>
+          </NextLink>
+        </Box>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default EmptyPage;
