ff35dbab588b4b1b7ca843d7de85744de86d3030
Mario <luix30lp@gmail.com>
2022-12-18 20:20:25 -0600

    Added swr dependency; get products from db

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,23 @@ import type { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts';
 import { initialData } from '../database/products';
 import { ProductList } from '../components/products/ProductList';
+import { useProducts } from '../hooks';
 
 const Home: NextPage = () => {
-  return (
-    <ShopLayout
-      title={'Teslo-shop - Home'}
-      pageDescription={'Find the best products'}
-    >
-      <Typography variant="h1" component="h1">
-        Store
-      </Typography>
-      <Typography variant="h2" component="h2">
-        All products
-      </Typography>
+	const { products, isLoading, isError } = useProducts('/products');
 
-      <ProductList products={initialData.products as any} />
-    </ShopLayout>
-  );
+	return (
+		<ShopLayout title={'Teslo-shop - Home'} pageDescription={'Find the best products'}>
+			<Typography variant="h1" component="h1">
+				Store
+			</Typography>
+			<Typography variant="h2" component="h2">
+				All products
+			</Typography>
+
+			{isLoading ? <h1>Loading...</h1> : <ProductList products={products} />}
+		</ShopLayout>
+	);
 };
 
 export default Home;
