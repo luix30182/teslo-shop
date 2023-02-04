a313e752d71e0e0317dd60bdff8e619f7ded1f30
Mario <luix30lp@gmail.com>
2023-02-04 14:36:26 -0600

    Added auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,26 @@ import { SWRConfig } from 'swr';
 import { UiProvider } from '@/context/ui/uiProvider';
 import { CartProvider } from '@/context/cart/CartProvider';
 import '../styles/globals.css';
+import { AuthProvider } from '../context/auth/AuthProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<SWRConfig
 			value={{
-				fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+				fetcher: (resource, init) =>
+					fetch(resource, init).then(res => res.json())
 			}}
 		>
-			<CartProvider>
-				<UiProvider>
-					<ThemeProvider theme={lightTheme}>
-						<CssBaseline />
-						<Component {...pageProps} />
-					</ThemeProvider>
-				</UiProvider>
-			</CartProvider>
+			<AuthProvider>
+				<CartProvider>
+					<UiProvider>
+						<ThemeProvider theme={lightTheme}>
+							<CssBaseline />
+							<Component {...pageProps} />
+						</ThemeProvider>
+					</UiProvider>
+				</CartProvider>
+			</AuthProvider>
 		</SWRConfig>
 	);
 }
